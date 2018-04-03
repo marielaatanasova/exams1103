f4b8246accf6e017b746eac6da3882af5e8ac1c6
Mariela Atanasova <mariela_2001at@abv.bg>
2018-04-03 10:26:36 +0300

    exams1103

---
+++ p01CryptoSolution.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p01CryptoSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal bitcoin = decimal.Parse(Console.ReadLine());
+            decimal ethereum = decimal.Parse(Console.ReadLine());
+            decimal litecoin = decimal.Parse(Console.ReadLine());
+            short tranzakcii = short.Parse(Console.ReadLine());
+            decimal[] sums = new decimal[tranzakcii];
+            string[] valuta = new string[tranzakcii];
+            string[] operation = new string[tranzakcii];
+            for (int i = 0; i < tranzakcii; i++)
+            {
+                sums[i] = decimal.Parse(Console.ReadLine());
+                valuta[i] = Console.ReadLine();
+                operation[i] = Console.ReadLine();
+            }
+            decimal Totalsustoqnie = 0;
+            
+            decimal[] sustoqniq = new decimal[tranzakcii];
+            decimal[] komersialni = new decimal[tranzakcii];
+            for (int j = 0; j < tranzakcii; j++)
+            {
+                decimal sustoqnie = 0;
+                decimal komers = 0;
+                if (valuta[j] == "Bitcoin")
+                {
+                    sustoqnie = sums[j] * bitcoin;
+                }
+                else if (valuta[j] == "Ethereum")
+                {
+                    sustoqnie = sums[j] * ethereum;
+                }
+                else if (valuta[j] == "Litecoin")
+                {
+                    sustoqnie = sums[j] * litecoin;
+                }
+                sustoqniq[j] = sustoqnie;
+                komers = sustoqnie * 0.073456764216789345M;
+                komersialni[j] = komers;
+
+            }
+            for (int i = 0; i < tranzakcii; i++)
+            {
+                if (operation[i] == "Buy")
+                {
+                    Totalsustoqnie += sustoqniq[i];
+
+                }
+                else
+                {
+                    Totalsustoqnie -= sustoqniq[i];
+                }
+            }
+           
+            Console.WriteLine($"{(Totalsustoqnie - komersialni.Sum()):f16}");
+        }
+    }
+}
---
+++ p02Commands.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem2
+{
+    class p02Commands
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            string command = Console.ReadLine();
+            List<string> allCommands = new List<string>();
+            while (command != "print")
+            {
+                allCommands.Add(command);
+                command = Console.ReadLine();
+            }
+            List<string> space = new List<string>();
+            int el = 0;
+            for (int i = 0; i < allCommands.Count; i++)
+            {
+                space = allCommands[i].Split(' ').ToList();
+                string komanda = space[0];
+                if (komanda == "push")
+                {
+                    el = int.Parse(space[1]);
+                    numbers.Add(el);
+
+                }
+                else
+                {
+                    if (komanda == "pop")
+                    {
+                        el = numbers[numbers.Count - 1];
+                        Console.WriteLine(el);
+                        numbers.Remove(el);
+                    }
+                    else
+                    {
+                        if (komanda == "shift")
+                        {
+                            el = numbers[numbers.Count - 1];
+                            numbers[numbers.Count - 1] = numbers[0];
+                            numbers[0] = el;
+
+                        }
+                        else
+                        {
+                            if (komanda == "addMany")
+                            {
+                                el = int.Parse(space[1]);
+                                List<int> addmany = new List<int>();
+                                for (int j = 2; j < space.Count; j++)
+                                {
+                                    addmany.Add(int.Parse(space[j]));
+                                }
+                                if (el > 0 && el < numbers.Count)
+                                {
+                                    numbers.InsertRange(el, addmany);
+                                }
+                            }
+                            else
+                            {
+                                if (komanda == "remove")
+                                {
+                                    el = int.Parse(space[1]);
+                                    if (el > 0 && el < numbers.Count)
+                                    {
+                                        numbers.RemoveAt(el);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            numbers.Reverse();
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+
+    }
+}
+
+
